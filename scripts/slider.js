9f86e1a6e6675c966f9a593f9027190bb3f0ed16
Hal-Xing <xingyuehao222@gmail.com>
2024-01-19 15:14:46 +0900

    Update slider.js

--- scripts/slider.js
+++ scripts/slider.js
@@ -24,26 +24,41 @@ function handleKeydown(e) {
 function handleTouch(e) {
     const slider = document.querySelector('.slider');
     const slides = slider.getElementsByClassName('slide');
+    const lightbox = document.getElementById('lightbox');
 
     if (e.type === 'touchstart') {
         startX = e.touches[0].clientX;
     } else if (e.type === 'touchmove') {
         endX = e.touches[0].clientX;
     } else if (e.type === 'touchend') {
-        if (Math.abs(startX - endX) < 10) {  // If the touch movement was less than 10px, it's a tap
-            // Find the tapped slide
-            for (let i = 0; i < slides.length; i++) {
-                if (slides[i].contains(e.target)) {
-                    currentSlide = i;
-                    break;
-                }
+        if (lightboxVisible) {  // If the lightbox is visible
+            if (startX < endX) {  // Swipe right
+                currentSlide = Math.max(currentSlide - 1, 0);  // Don't go below 0
+            } else if (startX > endX) {  // Swipe left
+                currentSlide = (currentSlide + 1) % slides.length;  // Loop back to the first image
             }
-        } else if (startX < endX) {  // Swipe right
-            currentSlide = Math.max(currentSlide - 1, 0);  // Don't go below 0
-        } else if (startX > endX) {  // Swipe left
-            currentSlide = (currentSlide + 1) % slides.length;  // Loop back to the first image
+            lightbox.style.backgroundImage = `url(${slides[currentSlide].src})`;
+        } else {  // If the lightbox is not visible
+            if (Math.abs(startX - endX) < 10) {  // If the touch movement was less than 10px, it's a tap
+                // Find the tapped slide
+                for (let i = 0; i < slides.length; i++) {
+                    if (slides[i].contains(e.target)) {
+                        currentSlide = i;
+                        break;
+                    }
+                }
+                // Add the 'enlarged' class to the tapped slide and remove it from all other slides
+                for (let slide of slides) {
+                    slide.classList.remove('enlarged');
+                }
+                slides[currentSlide].classList.add('enlarged');
+            } else if (startX < endX) {  // Swipe right
+                currentSlide = Math.max(currentSlide - 1, 0);  // Don't go below 0
+            } else if (startX > endX) {  // Swipe left
+                currentSlide = (currentSlide + 1) % slides.length;  // Loop back to the first image
+            }
+            navigateSlides(null, slides);
         }
-        navigateSlides(null, slides);
     }
 }
 
